89cf12d100c4a0e0843b1059ab0c1ef05e36a137
amarshall20 <33075034+amarshall20@users.noreply.github.com>
2018-01-28 19:11:30 -0700

    Initial commit

---
+++ MinimalMVVM/MinimalMVVM/ViewModels/Presenter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using MinimalMVVM.Models;
+
+namespace MinimalMVVM.ViewModels
+{
+    public class Presenter : ObservableObject
+    {
+        private readonly TextConverter _textConverter = new TextConverter(s => s.ToUpper());
+        private string _someText;
+        private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+
+        public string SomeText
+        {
+            get { return _someText; }
+            set
+            {
+                _someText = value;
+                RaisePropertyChangedEvent("SomeText");
+            }
+        }
+
+        public IEnumerable<string> History
+        {
+            get { return _history; }
+        }
+
+        public ICommand SaveTextCommand
+        {
+            get { return new DelegateCommand(SaveText); }
+        }
+
+        private void SaveText()
+        {
+            if (string.IsNullOrWhiteSpace(SomeText)) return;
+            AddToHistory(_textConverter.SaveText(SomeText));
+            SomeText = string.Empty;
+        }
+
+        private void AddToHistory(string item)
+        {
+            if (!_history.Contains(item))
+                _history.Add(item);
+        }
+    }
+}
